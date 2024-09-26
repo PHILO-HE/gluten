4c89a837c68251bdd81d04732fa7a833305ecf61
PHILO-HE <he_feilong@126.com>
2024-09-26 21:32:42 +0800

    Initial

--- cpp/CMake/Findjemalloc_pic.cmake
+++ cpp/CMake/Findjemalloc_pic.cmake
@@ -15,24 +15,21 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# Find Jemalloc
 macro(find_jemalloc)
-  # Find the existing jemalloc
-  set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
-  # Find from vcpkg-installed lib path.
+  set(SHARED_LIBRARY_SUFFIX ".so")
+  set(LIB_NAME "jemalloc")
+  set(LIB_FULL_NAME
+          ${CMAKE_SHARED_LIBRARY_PREFIX}${LIB_NAME}${SHARED_LIBRARY_SUFFIX})
   find_library(
     JEMALLOC_LIBRARY
-    NAMES jemalloc_pic
-    PATHS
-      ${CMAKE_CURRENT_BINARY_DIR}/../../../dev/vcpkg/vcpkg_installed/x64-linux-avx/lib/
-    NO_DEFAULT_PATH)
+    NAMES ${LIB_FULL_NAME})
   if("${JEMALLOC_LIBRARY}" STREQUAL "JEMALLOC_LIBRARY-NOTFOUND")
     message(STATUS "Jemalloc Library Not Found.")
     set(JEMALLOC_NOT_FOUND TRUE)
   else()
     message(STATUS "Found jemalloc: ${JEMALLOC_LIBRARY}")
     find_path(JEMALLOC_INCLUDE_DIR jemalloc/jemalloc.h)
-    add_library(jemalloc::libjemalloc STATIC IMPORTED)
+    add_library(jemalloc::libjemalloc SHARED IMPORTED)
     set_target_properties(
       jemalloc::libjemalloc
       PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${JEMALLOC_INCLUDE_DIR}"
