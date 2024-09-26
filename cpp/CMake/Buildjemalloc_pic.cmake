9f2303f8a30fc8cec72604c5f684346d82592c36
PHILO-HE <he_feilong@126.com>
2024-09-27 01:05:27 +0800

    Fix linking issue

--- cpp/CMake/Buildjemalloc_pic.cmake
+++ cpp/CMake/Buildjemalloc_pic.cmake
@@ -41,9 +41,11 @@ macro(build_jemalloc)
       "CC=${CMAKE_C_COMPILER}"
       "--prefix=${JEMALLOC_PREFIX}"
       "--libdir=${JEMALLOC_LIB_DIR}"
+      # The below prefix can be enabled if jemalloc is used in some selective code for debugging or profiling.
       # "--with-jemalloc-prefix=je_gluten_"
       # "--with-private-namespace=je_gluten_private_"
-      "--without-export"
+      # This should be commented for dynamically linking.
+      # "--without-export"
       "--disable-cxx"
       "--disable-libdl"
       # For fixing an issue when loading native lib: cannot allocate memory in
